89e77e14067393972b9b5909135d09ec935b4a55
millerbermeo <millerbermeo30@gmail.com>
2023-10-28 13:30:18 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -1,45 +1,73 @@
+//variables 
+
 let btn1 = document.querySelector(".menu-amb ")
 let sidebar = document.querySelector(".sidebar-menu ")
 let home = document.querySelector(".dashboard-main")
+let btnForm = document.querySelector("#userForm")
+let bodyElement = document.querySelector(".dashboard-main");
+let overlay = document.querySelector(".overlay")
+
 
 let btnInfo = document.querySelector(".btn-options")
 let contentInfo = document.querySelector(".content-options")
 
-btnInfo.addEventListener("click", ()=> {
-    contentInfo.classList.toggle("active")
+
+//funcion para abrir y cerrar modal de opciones
+
+btnInfo.addEventListener("click", () => {
+    if (contentInfo.classList.contains("active")) {
+        contentInfo.classList.remove("active")
+    } else {
+        contentInfo.classList.add("active")
+    }
+})
+bodyElement.addEventListener("click", () => {
+    if (contentInfo.classList.contains("active")) {
+        contentInfo.classList.remove("active")
+    }
 })
 
+//funcion para abrir y cerrar el sidebar
 
-btn1.addEventListener("click", ()=> {
+btn1.addEventListener("click", () => {
+
     sidebar.classList.toggle("active")
     home.classList.toggle("active")
+    overlay.classList.toggle("active")
 })
 
+
+
+//funcion para insertar formulario abrir y mostrar usuarios usando un for
 
 const mostrarFormularioBtn = document.getElementById("mostrarFormulario");
 const formulario = document.getElementById("formulario");
 const userForm = document.getElementById("userForm");
 
-let isFormRendered = false;
-let isFormOpen = false; 
+let renderForm = false;
+let openForm = false;
 
-mostrarFormularioBtn.addEventListener("click", function() {
-    if (!isFormRendered) {
+//mostrar el formulario
+
+mostrarFormularioBtn.addEventListener("click", function () {
+    if (!renderForm) {
         renderizarUsuarios();
-        isFormRendered = true;
+        renderForm = true;
     }
-    if (!isFormOpen) {
+    if (!openForm) {
         formulario.style.display = "block";
     }
 });
 
+//mostrar los usuarios usando un for, con createElemnt creo elementos y contendeores para alojar la informacion
+
 function renderizarUsuarios() {
-    // Crear un div para contener los checkboxes
+
     const checkboxesContainer = document.createElement("div");
-    
+
     for (let i = 1; i <= 10; i++) {
         const div = document.createElement("div");
-        div.classList.add("form-container-item"); // Agregar la clase CSS
+        div.classList.add("form-containe");
 
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
@@ -57,39 +85,59 @@ function renderizarUsuarios() {
         checkboxesContainer.appendChild(document.createElement("br"));
     }
 
-    // Agregar el contenedor de los checkboxes al formulario
     userForm.appendChild(checkboxesContainer);
 
-    // Crear el botón "Seleccionar Todos" fuera del contenedor de los checkboxes
+
+    //crear botom para ejecutar funcion de seleccionar todos los checkbox
+
     const selectAllBtn = document.createElement("button");
+
     selectAllBtn.textContent = "Seleccionar Todos";
     selectAllBtn.setAttribute("class", "btn-all");
+    let allSelected = false;
 
-    selectAllBtn.addEventListener("click", function(event) {
+    selectAllBtn.addEventListener("click", function (event) {
         event.preventDefault();
         const checkboxes = checkboxesContainer.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = true;
-        });
-        checkAllCheckboxes();
-    });
-
-    // Agregar el botón fuera del contenedor de los checkboxes
-    userForm.appendChild(selectAllBtn);
-}
-
-function checkAllCheckboxes() {
-    const checkboxes = userForm.querySelectorAll('input[type="checkbox"]');
-    let allChecked = true;
-
-    checkboxes.forEach(checkbox => {
-        if (!checkbox.checked) {
-            allChecked = false;
-            return;
+        if (allSelected) {
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = false;
+            });
+            allSelected = false;
+        } else {
+            checkboxes.forEach(checkbox => {
+                checkbox.checked = true;
+            });
+            allSelected = true;
         }
     });
 
-    // Aquí puedes agregar la lógica para cuando se seleccionen todos los checkboxes
+    userForm.appendChild(selectAllBtn);
 }
+
+
+
+//tomar los datos del form y mostralos en un alert
+
+userForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const checkboxes = userForm.querySelectorAll('input[type="checkbox"]');
+    const datosSeleccionados = [];
+
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            datosSeleccionados.push(checkbox.getAttribute('name'));
+        }
+    });
+
+    if (datosSeleccionados.length > 0) {
+        alert("Checkbox seleccionados:\n" + datosSeleccionados.join("\n"));
+    } else {
+        alert("Ningún checkbox seleccionado.");
+    }
+});
+
+
 
 
