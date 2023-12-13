067bad55f40a8496cdd013f414b94813203ef133
millerbermeo <millerbermeo30@gmail.com>
2023-12-13 16:59:36 -0500

    api prueba

--- js/campanas.js
+++ js/campanas.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiUrl = '';
+
+    //ESTA ES UNA API LOCAL QUE HICE ELLA RESPONDE CON id, nombre, mensaje y url
+
+    const apiUrl = 'http://127.0.0.1:8000/api/campana';
 
     fetch(apiUrl)
         .then(response => {
@@ -10,9 +13,24 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             console.log('Datos recibidos:', data);
-
+            displayDataInTable(data);
         })
         .catch(error => {
             console.error('Error en la solicitud:', error);
         });
+
+    function displayDataInTable(data) {
+        const tableBody = document.getElementById('table-body');
+
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.nombre}</td>
+                <td>${item.mensaje}</td>
+                <td><a class="url-link" href="${item.url}">${item.url}</a></td>
+            `;
+            tableBody.appendChild(row);
+        });
+    }
 });
